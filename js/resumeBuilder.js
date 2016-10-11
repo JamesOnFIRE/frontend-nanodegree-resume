f34bf62288540e1045120977826d6bd0093004bf
James Talbot <jamestalbot64@yahoo.com>
2016-10-10 23:48:07 -0700

    Added my name and role to the interactive resume.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+
+var formattedName = HTMLheaderName.replace("%data%","James Talbot");
+
+var formattedRole = HTMLheaderRole.replace("%data%","Front End Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
